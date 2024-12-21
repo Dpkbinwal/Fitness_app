2a0c070066c777045b0e585e709a224263602fcf
Pradeep Binwal <Pradeepbinwal1@gmail.com>
2024-12-21 23:43:55 +0530

    package build

--- src/index.js
+++ src/index.js
@@ -7,10 +7,11 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 
-const root= ReactDOM.createRoot(document.getElementById('root'));
+// const root=  ReactDOM.createRoot(document.getElementById('root'));  
 
-root.render(
+ReactDOM.render(
     <BrowserRouter>
     <App/>
-    </BrowserRouter> 
+    </BrowserRouter> ,
+  document.getElementById('root')
 );
